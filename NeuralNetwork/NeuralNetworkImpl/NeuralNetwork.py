23ef95dd121746e04c0addf366283a09c62666b9
Matthew Lee <matthew.lee711@gmail.com>
2017-04-17 21:26:55 -0500

    Added Chatbot training and neural network code - gotta test

---
+++ NeuralNetwork/NeuralNetworkImpl/NeuralNetwork.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn
+import sklearn.datasets
+import sklearn.linear_model
+import matplotlib
+
+class NeuralNetwork():
+    def __init__(self):
+        # Define dimensionality of NN
+        self.inputLayerSize = 2
+        self.outputLayerSize = 1
+        self.hiddenLayerSize = 3
+        # Gradient descent parameters
+        self.epsilon = 0.01        # learning rate for gradient descent
+        self.reg_lambda = 0.01     # regularization strength
+
+    # Forward propagation
+    def forward(self, X):
+        self.z1 = X.dot(W1) + b1
+        self.a1 = sigmoid(z1)
+        self.z2 = a1.dot(W2) + b2
+        self.yHat = sigmoid(z2)
+        return yHat
+
+    # Sigmoid activation function
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    #  Derivative of sigmoid
+    def sigmoidPrime(self, x):
+        return np.exp(-x)/((1+np.exp(-x))**2)
+
+    # Rate of change of y(hat) with respect to output
+    # Compute cost for given X,y, use weights already stored in class.
+    def costFunction(self, X, y):
+        self.yHat = self.forward(X)
+        J = 0.5*sum((y-self.yHat)**2)
+        return J
+
+    def costFunctionPrime(self, X, y):
+        #Compute derivative with respect to W and W2 for a given X and y:
+        self.yHat = self.forward(X)
+
+        # Backpropagation on layers 2 and 3
+        delta3 = np.multiply(-(y-self.yHat), self.sigmoidPrime(self.z3))
+        dJdW2 = np.dot(self.a2.T, delta3)
+        db2 = np.sum(delta3, axis=0, keepdims=True)
+
+        # Backpropagation on layers 1 and 2
+        delta2 = np.dot(delta3, self.W2.T)*self.sigmoidPrime(self.z2)
+        dJdW1 = np.dot(X.T, delta2)
+        db1 = np.sum(delta2, axis=0)
+
+        return dJdW1, dJdW2, db1, db2
+
+    def build_model(self, X, y, nn_hdim, num_passes=20000):
+        # Setting Random weights and bias
+        self.W1 = np.random.randn(nn_input_dim, nn_hdim) / np.sqrt(nn_input_dim)
+        self.W2 = np.random.randn(nn_hdim, nn_output_dim) / np.sqrt(nn_hdim)
+        self.b1 = np.zeros((1, nn_hdim))
+        self.b2 = np.zeros((1, nn_output_dim))
+        model = {}
+
+        # Build the nn model
+        for i in xrange(0, 20000):
+            # Perform gradientDescent
+            dJdW1, dJdW2, db1, db2 = costFunctionPrime(X, y)
+
+            # Regularization
+            dJdW2 += reg_lambda * W2
+            dJdW1 += reg_lambda * W1
+
+            # Gradient descent parameter update
+            W1 += -epsilon * dJdW1
+            b1 += -epsilon * db1
+            W2 += -epsilon * dJdW2
+            b2 += -epsilon * db2
+
+            model = { 'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2}
+
+        return model
+
+
+def main():
+    createNN = NeuralNetwork()
